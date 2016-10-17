06a6e2e7f399fe574141ee6bbfbcd4969c3626bf
Frank <fjlanasa@gmail.com>
2016-10-17 14:03:11 -0400

    added user unit test

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,6 +10,6 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :trackable, :validatable
 
   def full_name
-    "#{:first_name} #{:last_name}"
+    "#{first_name} #{last_name}"
   end
 end
