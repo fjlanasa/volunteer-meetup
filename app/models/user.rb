f2dad4ed9c190e33c84b8d1be318fb93512a4500
Frank <fjlanasa@gmail.com>
2016-10-17 09:14:18 -0400

    added phone number field to user and updated tests accordingly

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   validates :first_name, presence: true
   validates :last_name, presence: true
+  validates :phone_number, presence: true
+  validates :phone_number, format: { with: /\A[0-9]{3}-[0-9]{3}-[0-9]{4}\z/, message: 'must be in format ###-###-####' }
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 end
