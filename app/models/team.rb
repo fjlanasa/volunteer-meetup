6dd786bcfe9b43d066257a3fe95c2562b3b78235
Frank <fjlanasa@gmail.com>
2016-10-19 08:50:43 -0400

    removed validations for deleted columns in teams

--- app/models/team.rb
+++ app/models/team.rb
@@ -2,8 +2,6 @@ class Team < ActiveRecord::Base
   validates :meeting_location, presence: true
   validates :meeting_time, presence: true
   validates :user_id, presence: true
-  validates :workers_needs, presence: true
-  validates :supplies_needed, presence: true
 
   belongs_to :user
   has_many :signups
