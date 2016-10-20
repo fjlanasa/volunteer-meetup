1661d8aacfe2fc0771ddbbfeccbb4c86dca75557
Frank <fjlanasa@gmail.com>
2016-10-20 11:55:19 -0400

    changed sign-up test to address deletion of volunteers

--- spec/models/signup_spec.rb
+++ spec/models/signup_spec.rb
@@ -9,26 +9,22 @@ RSpec.describe Signup, type: :model do
     User.create(first_name: 'Krystal', last_name: 'Cantos', email: 'kvc@gmail.com', password: 'password', phone_number: '111-111-1111')
   }
 
-  let(:volunteer){
-    Volunteer.create(user_id: user1.id, labor: true, supplies: false)
-  }
-
   let(:team){
-    Team.create(user_id: user2.id, meeting_location: 'Home', meeting_time: 'Now!')
+    Team.create(organizer_id: user2.id, meeting_location: 'Home', meeting_time: 'Now!')
   }
 
   let(:signup){
-    Signup.create(team_id: team.id, volunteer_id: volunteer.id)
+    Signup.create(team_id: team.id, user_id: user1.id)
   }
 
   it 'has the expected properties' do
-    expect(signup.volunteer.first_name).to eq('Frank')
-    expect(signup.volunteer.last_name).to eq('LaNasa')
-    expect(signup.volunteer.email).to eq('fjl@gmail.com')
-    expect(signup.volunteer.phone_number).to eq('111-111-1111')
-    expect(signup.volunteer.full_name).to eq('Frank LaNasa')
-    expect(signup.volunteer.labor).to eq(true)
-    expect(signup.volunteer.supplies).to eq(false)
+    expect(signup.user.first_name).to eq('Frank')
+    expect(signup.user.last_name).to eq('LaNasa')
+    expect(signup.user.email).to eq('fjl@gmail.com')
+    expect(signup.user.phone_number).to eq('111-111-1111')
+    expect(signup.user.full_name).to eq('Frank LaNasa')
+    expect(signup.user.labor).to eq(false)
+    expect(signup.user.supplies).to eq(false)
     expect(signup.team.meeting_location).to eq('Home')
   end
 end
