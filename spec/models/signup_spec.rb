cbb8859f392c71c1a29f8c469837ece01500a937
Frank <fjlanasa@gmail.com>
2016-10-21 11:04:23 -0400

    fixed signup test

--- spec/models/signup_spec.rb
+++ spec/models/signup_spec.rb
@@ -9,8 +9,12 @@ RSpec.describe Signup, type: :model do
     User.create(first_name: 'Krystal', last_name: 'Cantos', email: 'kvc@gmail.com', password: 'password', phone_number: '111-111-1111')
   }
 
+  let(:site){
+    Site.create(user_id: user1.id, location: 'Home', contact_name: user1.full_name, contact_phone: user1.phone_number, square_footage: '1000')
+  }
+
   let(:team){
-    Team.create(organizer_id: user2.id, meeting_location: 'Home', meeting_time: 'Now!')
+    Team.create(organizer_id: user2.id, meeting_location: 'Home', meeting_time: 'Now!', site_id: site.id)
   }
 
   let(:signup){
@@ -18,6 +22,7 @@ RSpec.describe Signup, type: :model do
   }
 
   it 'has the expected properties' do
+    binding.pry
     expect(signup.user.first_name).to eq('Frank')
     expect(signup.user.last_name).to eq('LaNasa')
     expect(signup.user.email).to eq('fjl@gmail.com')
