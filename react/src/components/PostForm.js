20bb5fcdca129cef0fe8d62c535a819de85ef6d7
Frank <fjlanasa@gmail.com>
2016-10-24 14:04:40 -0400

    styling

--- react/src/components/PostForm.js
+++ react/src/components/PostForm.js
@@ -2,18 +2,20 @@ import React from 'react';
 
 const PostForm = props => {
   return(
-    <div className='small 12 columns callout'>
-      <form onSubmit={props.onSubmit} id='post-form'>
-        <div className='input group'>
-          <label htmlFor="post-text">Leave a Post</label>
-          <textarea id="post-text" type="textarea" rows="3" cols="20" wrap="hard"
-           className="input-group-field" name="post_text" onChange={props.onChange}
-           value={props.post_text}/>
-          <div className="input-group-button">
-            <input type="submit" className="button" value="Submit"/>
+    <div className='small-12 columns'>
+      <div className='small 12 columns callout'>
+        <form onSubmit={props.onSubmit} id='post-form'>
+          <div className='input group'>
+            <label htmlFor="post-text">Leave a Post</label>
+            <textarea id="post-text" type="textarea" rows="3" cols="20" wrap="hard"
+             className="input-group-field" name="post_text" onChange={props.onChange}
+             value={props.post_text}/>
+            <div className="input-group-button">
+              <input type="submit" className="button" value="Submit"/>
+            </div>
           </div>
-        </div>
-      </form>
+        </form>
+      </div>
     </div>
   )
 }
